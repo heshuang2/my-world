6f84b64a260d5fe8a2ef590a43017e835e61f2f8
heshuang <hx1323@yeah.net>
2018-11-13 18:54:36 +0800

    我的世界

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public GameObject bodyInventory;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(bodyInventory.activeInHierarchy == true)
+            {
+                bodyInventory.SetActive(false);
+            }
+            else
+            {
+                bodyInventory.SetActive(true);
+            }
+
+        }
+                
+		
+	}
+}
